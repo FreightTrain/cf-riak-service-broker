8b0f5346ee3376ccb7feb193eb293deaa9e3f297
Hector Castro <hectcastro@gmail.com>
2013-10-26 21:50:05 -0400

    Persist service bindings and instances in Riak.

--- lib/service_instances.rb
+++ lib/service_instances.rb
@@ -1,45 +1,40 @@
-module RiakBroker
-  SERVICE_INSTANCES = { }
+require "riak"
 
+module RiakBroker
   class ServiceInstances < Sinatra::Base
     before do
       content_type "application/json"
+
+      @client = Riak::Client.new(nodes: [ { host: CONFIG["riak_hosts"].sample, http_port: 8098 } ])
+      @service_instances = @client.bucket("service_instances")
     end
 
     helpers do
-      def set_backend_bucket_prop(backend, bucket_uuid)
-        bucket_props = { "props" => { "backend" => backend } }
-
-        RestClient.put(
-          "http://#{CONFIG["riak_hosts"].sample}:8098/buckets/#{bucket_uuid}/props",
-          bucket_props.to_json,
-          content_type: :json,
-          accept: :json
-        )
-      end
-
       def set_backend(plan_id, bucket_uuid)
+        bucket = @client.bucket(bucket_uuid)
+
         if plan_id == BITCASK_PLAN_ID
-          set_backend_bucket_prop("bitcask_mult", bucket_uuid)
+          bucket.properties = { "backend" => "bitcask_mult" }
         elsif plan_id == LEVELDB_PLAN_ID
-          set_backend_bucket_prop("eleveldb_mult", bucket_uuid)
+          bucket.properties = { "backend" => "eleveldb_mult" }
         end
       end
 
       def add_service(service_id, plan_id)
-        SERVICE_INSTANCES[service_id] = {
-          plan_id: plan_id,
-        }
+        object = @service_instances.get_or_new(service_id)
+        object.content_type = "application/json"
+        object.data = { plan_id: plan_id }.to_json
+        object.store({ returnbody: false })
 
         set_backend(plan_id, service_id)
       end
 
       def remove_service(service_id)
-        SERVICE_INSTANCES.delete(service_id)
+        @service_instances.delete(service_id)
       end
 
       def already_provisioned?(service_id)
-        SERVICE_INSTANCES.key?(service_id)
+        @service_instances.exist?(service_id)
       end
     end
 
