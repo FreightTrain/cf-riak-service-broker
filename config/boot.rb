c9ef5a36f78bd302e7b7ea684fc751d3e503a75d
Andrew Crump and Tommaso Carrara <freightrain@cisco>
2014-06-04 17:07:53 +0100

    Allow config file to be passed as an ENV variable
    [#71721544]

--- config/boot.rb
+++ config/boot.rb
@@ -9,7 +9,9 @@ Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 Dir["./lib/**/*.rb"].each { |f| require f }
 
 if File.exists?("config/broker.yml")
-  CONFIG = YAML.load(File.read("config/broker.yml"))
+  CONFIG = YAML.load_file("config/broker.yml")
+elsif ENV['BROKER_CONFIG_FILE']
+  CONFIG = YAML.load_file(ENV['BROKER_CONFIG_FILE'])
 else
   $stderr.puts "ERROR: No broker.yml file found in configuration directory."
   exit(1)
